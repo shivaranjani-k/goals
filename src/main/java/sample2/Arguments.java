15762a78deb0e147b75c021d6c72d168591688f7
csk <shivaranjani229@gmail.com>
2019-05-02 22:32:07 +0530

    push to github

---
+++ src/main/java/sample2/Arguments.java
@@ -0,0 +1,8 @@
+package sample2;
+class Arguments {
+	  public static void main(String[] args) {
+	    for (String t: args) {
+	      System.out.println(t);
+	    }
+	  }
+	}
---
+++ src/test/java/sample2/sundaytest.java
@@ -0,0 +1,14 @@
+package sample2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sundaytest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
